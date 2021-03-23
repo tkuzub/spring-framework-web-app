860f3c43e00d9a94de5cecda50bae887ffe075b7
Taras Kuzub <taras.kuzub.ua@gmail.com>
2021-03-23 18:13:39 +0200

    Добавили фильтр

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -36,6 +37,18 @@ public class GreetingController {
         messageRepo.save(message);
 
         Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
         model.put("messages", messages);
         return "main";
     }
